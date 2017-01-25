260bcc997c5857b434f89736664fcac3527e37e1
abhay kumar singh <asingh9@searshc.com>
2017-01-25 14:03:51 -0600

    Initial Commit

---
+++ testFirst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testFirst
+//
+//  Created by Singh, Abhay on 1/25/17.
+//  Copyright © 2017 SHC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
